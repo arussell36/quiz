100963d8067cb4d9d457637eee92665532ce0943
arussell36 <andrewrussell036@gmail.com>
2019-10-08 18:28:18 -0500

    pseudocode

---
+++ script.js
@@ -0,0 +1,34 @@
+// VARIABLES,
+    // questions
+        // an array of questions
+        // question = string
+        // choices = array
+        // answer = string
+        // key = number (to store to LS)
+
+    // answers
+
+
+// HELPER FUNCTIONS
+
+
+
+// EVENTS
+    // 1. click start quiz button
+        // >> moves you to the first question (step3)
+
+    // 2. click high score button
+        // 2b. clear high scores button
+            // this clears local storage
+        // 2c. likely another button to bring you back to the main page
+
+    // 3. choose question answers
+        // >> parse the variables to be selectable answers
+        // >> stores answer in local storage and moves to next question
+        // >> also checks to see if answers now in local storage are correct
+
+    // 4. enter initials and submit score -> goes to high score page
+
+
+
+// INIT
