e63e2dbffbc0803bbd3d277c172a643d04e786e8
arussell36 <andrewrussell036@gmail.com>
2019-10-13 18:32:04 -0500

    looping through question functions

--- script.js
+++ script.js
@@ -3,13 +3,33 @@
 var questions = [
     {
         title: "1.) What is the overall end point/goal of the game?",
-        choices: ["to escape planet", "to colonize tribes", "to survey for resources", "to start a new world"],
-        answer: "to escape planet"
+        choices: ["A. to colonize tribes", "B. to escape planet", "C. to survey for resources", "D. to start a new world"],
+        answer: "B. to escape planet"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "2.) Unmodded what is the fastest growing pawn edible crop in Rimworld?",
+        choices: ["A. Corn", "B. Rice", "C. Potato", "D. Hay"],
+        answer: "B. Rice"
+    },
+    {
+        title: "3.) What is the currency used in Rimworld?",
+        choices: ["A. Gold", "B. Silver", "C. Coins", "D. Food"],
+        answer: "B. Silver"
+    },
+    {
+        title: "4.) Unmodded how many permanently hostile factions are there?",
+        choices: ["A. 0 Factions", "B. 1 Faction", "C. 2 Factions", "D. 3 Factions"],
+        answer: "B. 1 Faction"
+    },
+    {
+        title: "5.) Unmodded what is the first hostile entity that attacks your colony?",
+        choices: ["A. Another Faction", "B. A Maddened Animal", "C. An Activated Robot", "D. Native Wild People"],
+        answer: "B. A Maddened Animal"
+    },
+    {
+        title: "6.) How many days are in a quadrum",
+        choices: ["A. 10", "B. 15", "C. 25", "D. 50"],
+        answer: "B. 15"
     }];
 
 var questionCount = 0;
@@ -29,9 +49,9 @@ var questionCount = 0;
 // you suck checker alert lol
 function youSuck() {
     alert("yousuck");
-}
+};
 
-function startState() {
+function init() {
     // youSuck();
     $("#questionSpot").html("These questions will test your knowledge on surviving Rimworld. Topics that will be covered range from colony sustenance, hostile planet inhabitants, and R&D while you're on the field. Good luck!");
 };
@@ -42,47 +62,53 @@ function startState() {
 
 
 
-// EVENTS ---------------------------------------------------------------------------------------
+
+
+
+// EVENTS -------------------------------------------------------------------------------------------------
     // 1. click start quiz button
         // >> moves you to the first question (step3)  
-
-$("#startQuizButton").on("click", function(){
-    // youSuck();
-    $(".startStateButtons").empty();
-    $("#questionSpot").empty();
-    $("#quizHeader").empty();
-    // ^^^ all functions clear out the current spots to make way for the new data ---------------
-
-
-    var questionGen = $("<div>");
-    questionGen.addClass("questions");
-    questionGen.html(questions[0].title);
-    $("#questionSpot").append(questionGen);
-    
-
-    for (i = 0; i < questions[0].choices.length; i++) {
-
-        var answerTable = $("<div>");
-        answerTable.addClass("questionText");
-        $("#answerSpot").append(answerTable);
-        var answersGen = $("<button>");
-        answersGen.html("hoyooyoyyoy");
-        $(answerTable).append(answersGen);
-
-        
-    };
-        
-    
-   
-
-})
-
-
 
     // 2. click high score button
         // 2b. clear high scores button
             // this clears local storage
         // 2c. likely another button to bring you back to the main page
+
+
+
+$("#startQuiz").on("click", function() {
+    // youSuck();
+    
+
+    $(".startStateButtons").empty();
+    $("#questionSpot").empty();
+    $("#quizHeader").empty();
+    $("#answerSpot").empty();
+    // ^^^ all functions clear out the current spots to make way for the new data ------------------------
+
+    var questionGen = $("<div>").addClass("questions");
+    questionGen.html(questions[questionCount].title);
+    $("#questionSpot").append(questionGen);
+
+
+    for (i = 0; i < questions[questionCount].choices.length; i++) {
+
+        var answerTable = $("<div>").addClass("questionDiv");
+        $("#answerSpot").append(answerTable);
+        // creates a table that the new child elements can attach to with the correct formating -----------
+        var answersGen = $("<button>").addClass("questionText").attr("id", "nextQuestion");
+        answersGen.html(questions[questionCount].choices[i]);
+        $(answerTable).append(answersGen);
+
+
+        };      
+        questionCount = 1;
+        // console.log(questionCount);
+});
+
+
+
+    
 
     // 3. choose question answers
         // >> parse the variables to be selectable answers
@@ -95,4 +121,43 @@ $("#startQuizButton").on("click", function(){
 
 // INIT -----------------------------------------------------------------------------------------
 
-startState ();
+init();
+
+
+// QUESTIONS LOOPS ------------------------------------------------------------------------------
+
+$("#answerSpot").on("click","#nextQuestion", function() {
+    // youSuck();
+        
+    $("#questionSpot").empty();
+    $("#answerSpot").empty();
+
+    var questionGen = $("<div>").addClass("questions");
+    questionGen.html(questions[questionCount].title);
+    $("#questionSpot").append(questionGen);
+
+
+    for (i = 0; i < questions[questionCount].choices.length; i++) {
+
+        var answerTable = $("<div>").addClass("questionDiv");
+        $("#answerSpot").append(answerTable);
+        // creates a table that the new child elements can attach to with the correct formating -----------
+        var answersGen = $("<button>").addClass("questionText").attr("id", "nextQuestion");
+        answersGen.html(questions[questionCount].choices[i]);
+        $(answerTable).append(answersGen);
+
+
+        };      
+        questionCount = questionCount + 1;
+        console.log(questionCount);
+
+});
+
+
+
+
+
+
+ 
+
+
